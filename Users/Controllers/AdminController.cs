52917cef46b769a16edc419bbc8a9bdd82e79950
Brayan Kelly <the.script.bryan07@gmail.com>
2018-01-01 01:19:51 -0400

    Finish user crud

--- Users/Controllers/AdminController.cs
+++ Users/Controllers/AdminController.cs
@@ -2,13 +2,126 @@
 using System.Web.Mvc;
 using Users.Infrastructure;
 using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
+using Users.Models;
+using Microsoft.AspNet.Identity;
+using System;
 
 namespace Users.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index() => View(UserManager.Users);
+        public  ActionResult Index() => View(UserManager.Users);
+
+        public ActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser { UserName = model.Name, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded) { return RedirectToAction("Index"); }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await UserManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("Error", result.Errors);
+                }
+            }
+            else
+            {
+                return View("Error", new string[] { "User Not Found" });
+            }
+        }
+
+        public async Task<ActionResult> Edit(string id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult>Edit(string id, string email, string pass)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult validEmail = await UserManager.UserValidator.ValidateAsync(user);
+                if (!validEmail.Succeeded)
+                {
+                    AddErrorsFromResult(validEmail);
+                }
+
+                IdentityResult validPass = null;
+
+                if(pass != String.Empty)
+                {
+                    validPass = await UserManager.PasswordValidator.ValidateAsync(pass);
+
+                    if (validPass.Succeeded)
+                    {
+                        user.PasswordHash = UserManager.PasswordHasher.HashPassword(pass);
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(validPass);
+                    }
+                }
+                if((validEmail.Succeeded && validPass == null) || (validEmail.Succeeded &&
+                    validPass.Succeeded && pass != string.Empty))
+                {
+                    IdentityResult result = await UserManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    } else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User Not Found");
+                }
+            }
+            return View(user);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
 
         private AppUserManager UserManager => HttpContext.GetOwinContext().Get<AppUserManager>();
     }
