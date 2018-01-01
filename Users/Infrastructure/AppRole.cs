cca4791381d2d77f33ab1dabb4d494ba50809c05
Brayan Kelly <the.script.bryan07@gmail.com>
2018-01-01 15:53:20 -0400

    Finish user authentication

---
+++ Users/Infrastructure/AppRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Users.Infrastructure
+{
+    public class AppRole : IdentityRole
+    {
+        public int MyProperty { get; set; }
+    }
+}
